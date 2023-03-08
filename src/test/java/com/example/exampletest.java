31aeb05952928688e77c575b05766f18cdee6dd1
likhithayeturi06 <likhithayeturi06@gmail.com>
2023-03-08 10:06:19 +0530

    updated ExampleTest

--- src/test/java/com/example/exampletest.java
+++ src/test/java/com/example/exampletest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ExampleTest {
+class ExampleTest {
     
     @Test
     public void testAddition() {
