7a73d680532598ceb66006f65152222a8aaf17ba
likhithayeturi06 <likhithayeturi06@gmail.com>
2023-03-10 12:56:54 +0530

    added lint

--- src/main/java/com/frontbackend/springboot/Application.java
+++ src/main/java/com/frontbackend/springboot/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
 @SpringBootApplication(scanBasePackages={"com.*"})
-@ComponentScan(basePackages = {"PostsController","PostRepository"})
 public class Application {
 
     public static void main(String[] args) {
