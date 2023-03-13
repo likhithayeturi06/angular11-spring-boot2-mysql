1d7c8bc8f207e4ceac37c4f2f8ddce5788fc6509
likhithayeturi06 <likhithayeturi06@gmail.com>
2023-03-13 18:14:44 +0530

    modified some file

--- src/main/java/com/frontbackend/springboot/Application.java
+++ src/main/java/com/frontbackend/springboot/Application.java
@@ -4,10 +4,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
-@SpringBootApplication(scanBasePackages={"com.*"})
-public class Application {
+//@SpringBootApplication(scanBasePackages={"com.*"})
+//public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    //public static void main(String[] args) {
+    //    SpringApplication.run(Application.class, args);
+    //}
+//}
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@Configuration
+@ComponentScan
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
+  }
+
 }
